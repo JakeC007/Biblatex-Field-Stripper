066de4f398c8a46a4c63842cbb528234568b1670
Jake C <36495719+JakeC007@users.noreply.github.com>
2019-07-02 16:30:19 -0500

    Added list of things to strip

--- strip.py
+++ strip.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This file strips fields from a biblatex file. The orginal code was written by astrobel found here: 
 https://forums.zotero.org/discussion/22629/bibtex-export-request-option-to-omit-certain-fields-for-less-cluttered-bibliographies  
-I made the code more robust by modifying it to run from the command line, automatic outputfile creation, and taking in 
-multiple fields to strip.
+I made the code more robust by modifying it to run from the command line, automatic outputfile creation, giving options on what to strip 
+and taking in multiple fields to strip.
 2019 Jake Chanenson 
 """
 import sys
@@ -17,25 +17,46 @@ def main():
   inputF = raw_inputF + '.bib'
   outputF = raw_inputF + 'FIXED'+'.bib'
 
-  #Get user input on what to strip
-  q_flag = True
-  strip = []
-  while q_flag == True:
-     raw_strip = input("What would you like to strip? \n (press q to when you're done): ")
-     if raw_strip == "q":
-       break
-     strip.append(raw_strip)
-     print("Things to strip", end = " ")
-     print(strip)
+  lst_strip_options = getOptions(inputF)
 
-  file_found = " "
-  file_found = clean(inputF, outputF, strip)
-
-  if file_found == "quit": #if the IOexception was raised, handle it
+  if lst_strip_options == "quit": #handle IOError
     sys.exit
-  else: #if the input file was found
+
+  else: 
+    print("These are your options of what you can strip:\n {}".format(lst_strip_options[0:]))
+
+    #Get user input on what to strip
+    q_flag = True
+    strip = []
+    while q_flag == True:
+      raw_strip = input("What would you like to strip? \n (press q to when you're done): ")
+      if raw_strip == "q":
+        break
+      strip.append(raw_strip)
+      print("Things to strip", end = " ")
+      print(strip)
+
+    clean(inputF, outputF, strip)
+
     end_pos = len(strip)-1
     print("Cleaned {} from {} and writing to {}".format(strip[0:end_pos], inputF, outputF))
+
+def getOptions(inputF):
+  strip_options = []
+  try:
+    with open(inputF, 'r') as f:
+      for line in f:
+        temp_word = line.split(None, 1)
+        try:
+          if '@' not in temp_word[0] and '}' not in temp_word[0]:
+            strip_options.append(temp_word[0])
+        except: #edge case for blank lines
+          pass    
+  except IOError: #If the input file was not found
+    print("File not found. Please input the filename w/out the extension")
+    return "quit"
+  strip_lst = list(dict.fromkeys(strip_options)) #remove duplicates
+  return strip_lst      
 
 def clean(inputF, outputF, strip):
   filename = inputF # the input file
@@ -46,24 +67,20 @@ def clean(inputF, outputF, strip):
   
   flag = 0 
   tripped = False #flag to ensure that none of the desired fields have been found
-  try:
-    with open(filename) as infile, open(filename2, 'w+') as outfile:
-      for line in infile:
-        tripped = False
-        for word in strip:
-          start = word + ' = ' #current word to strip
-          if line.strip().startswith(start) == True:
-            flag = 1
-            tripped = True
-          elif flag == 1:
-              if line.strip().endswith(end) == True:
-                flag = 0               
-          else:
-            if word == "cpyflg" and tripped == False: 
-              outfile.write(line)
-  except IOError: #If the input file was not found
-    print("File not found. Please input the filename w/out the extension")
-    return "quit"          
+  with open(filename) as infile, open(filename2, 'w+') as outfile:
+    for line in infile:
+      tripped = False
+      for word in strip:
+        start = word + ' = ' #current word to strip
+        if line.strip().startswith(start) == True:
+          flag = 1
+          tripped = True
+        elif flag == 1:
+            if line.strip().endswith(end) == True:
+              flag = 0               
+        else:
+          if word == "cpyflg" and tripped == False: 
+            outfile.write(line)          
     
 if __name__ == "__main__":
   main()
